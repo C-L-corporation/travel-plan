03f10214d2ee5f635e8b0ec1ca6aeb0e924fe577
Lance Hsu <lancehsu12@gmail.com>
2023-07-13 22:22:39 +0800

    chore(server): eslint & prettier setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,13 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Travel plan server");
+app.get('/', (req, res) => {
+  res.send('Travel plan server');
 });
 
 app.listen(port, () => {
