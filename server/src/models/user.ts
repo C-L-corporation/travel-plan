9d33b109e09477a8038935c76b19f62019dc9f5f
Lance Hsu <lancehsu12@gmail.com>
2023-08-08 13:53:06 +0800

    chore: enhance user model

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -4,7 +4,7 @@ type IUser = {
   name: string;
   email: string;
   photo: string;
-  providers: { provider: string; userId: string }[];
+  providers: { provider: string; userId: string; username?: string }[];
   admin: boolean;
   plans: ReadonlyArray<ObjectId>;
   createdAt: Date;
@@ -15,12 +15,26 @@ const userSchema = new Schema<IUser>({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   photo: { type: String, default: null },
-  providers: { type: [{ provider: String, userId: String }], default: [] },
+  providers: {
+    type: [
+      {
+        provider: { type: String, required: true },
+        userId: { type: String, required: true },
+        username: { type: String, default: null },
+      },
+    ],
+    default: [],
+  },
   admin: { type: Boolean, default: false },
   plans: { type: [{ type: Schema.Types.ObjectId, ref: 'Plan' }], default: [] },
   createdAt: { type: Date, default: new Date() },
   updatedAt: { type: Date, default: null },
 });
+
+userSchema.index(
+  { 'providers.provider': 1, 'providers.userId': 1 },
+  { unique: true }
+);
 
 const User = model<IUser>('User', userSchema);
 
